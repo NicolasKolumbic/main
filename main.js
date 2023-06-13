7e0c0e3c4c6dfe2e7ffa323a1b8605efd1751939
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2023-06-13 19:03:39 -0300

    update titie text

--- main.js
+++ main.js
@@ -1,54 +1,36 @@
 var controls = document.querySelectorAll('.text-box input');
-/**
- * badInput
+var title = document.querySelector('#title');
 
-customError
+function updateTitle() {
+    title.textContent = 'Hola ' + this.value;
+}
 
-patternMismatch
-
-rangeOverflow
-
-rangeUnderflow
-
-stepMismatch
-
-tooLong
-
-tooShort
-
-typeMismatch
-
-valid
-
-valueMissing
-
- */
 function validate() {
     var message = '';
     if(this.validity.valueMissing) {
-       message =  `El campo "${this.nextElementSibling.textContent}" es requerido.`
+       message =  `El campo '${this.nextElementSibling.textContent}' es requerido.`
     } else if(this.validity.patternMismatch || this.validity.typeMismatch) {
-        message = `El formato del campo "${this.nextElementSibling.textContent}" es erroneo.${this.getAttribute('data-error-message')}`
+        message = `El formato del campo '${this.nextElementSibling.textContent}' es erroneo.${this.getAttribute('data-error-message')}`
     } else if(this.validity.tooShort) {
-        message = `El campo "${this.nextElementSibling.textContent}" debe tener como mínimo ${this.getAttribute('minLength')} caracteres`
+        message = `El campo '${this.nextElementSibling.textContent}' debe tener como mínimo ${this.getAttribute('minLength')} caracteres`
     }
 
     if(!this.validity.valid) {
-        if(!this.classList.contains("invalid")) {
-            this.classList.add("invalid");
+        if(!this.classList.contains('invalid')) {
+            this.classList.add('invalid');
         }
         this.parentElement.nextElementSibling.innerText = `❌ ${message}`;
     } else {
-        if(this.classList.contains("invalid")) {
-            this.classList.remove("invalid");
+        if(this.classList.contains('invalid')) {
+            this.classList.remove('invalid');
         } 
     }
 }
 
 function clear() {
     this.parentElement.nextElementSibling.innerText = '';
-    if(this.classList.contains("invalid")) {
-        this.classList.remove("invalid");
+    if(this.classList.contains('invalid')) {
+        this.classList.remove('invalid');
     } 
 }
 
@@ -56,3 +38,5 @@ controls.forEach(function(control) {
     control.addEventListener('blur', validate);
     control.addEventListener('focus', clear);
 })
+
+document.getElementById('fullName').addEventListener('input', updateTitle)
